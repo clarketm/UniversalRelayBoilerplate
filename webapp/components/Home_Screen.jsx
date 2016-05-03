6238721c35460f8bf92ea25c386ce2863f6f230e
Alex Chalakov <a.chalakov@codefoundries.com>
2016-05-02 19:04:04 -0700

    Remove information from home screen that might be obsolete

--- webapp/components/Home_Screen.jsx
+++ webapp/components/Home_Screen.jsx
@@ -27,59 +27,11 @@ class Home_Screen extends React.Component
             <li>To host examples of using those technologies together.</li>
             <li>To implement some common functionality like user authentication using react/relay while storing user accounts in Cassandra.</li>
           </ul>
-          Here is some of the functionality currently available in the project:
-          <ul>
-            <li>
-              <b>User authentication</b> : On the top right in the navigation bar, if the user is not logged in (i.e. is being anonymous) an
-              outline of a human bust is displayed.
-              Clicking on it opens the log in dialog.
-              If the user is already authenticated, a small pop over with information about the current user and user-related functions is displayed.
-            </li>
-            <li>
-              <b>Active tasks indicator</b> : If there are any un-completed To Do items, an indicator is displayed in the navigation bar with the
-              number of active tasks. Clicking on it takes the user to the active tasks.
-            </li>
-            <li>
-              <b>Navigation menu</b> : Clicking on the three vertical dots on the left of the navigation bar allows jumping to the different screens.
-            </li>
-            <li>
-              <b>Compendiums</b> : An example of a form for an one-per user record, for instance for per-user settings. The example features
-              multiple types of inputs and validation.
-            </li>
-            <li>
-              <b>Ensayo</b> : Simple example of list of essays, with Helpmet for SEO.
-            </li>
-          <li>
-              <b>MUI</b> : Examples focusing on Material-UI elements.
-            </li>
-            <li>
-              <b>To Do</b> : Interpretation of the ToDo example from React Relay, implemented with Material UI controls, and per-user
-              To Do items.
-            </li>
-            <li>
-              <b>Translaticiarums</b> : Simple per-user list of timed events. Allows for list view and daily agenda-like view.
-            </li>
-          </ul>
-          <a href="http://codefoundries.com/products/UniversalRelayBoilerplate.html">Project Home</a>.
+          <a href="http://codefoundries.com/products/UniversalRelayBoilerplate.html">PROJECT HOME</a>
           <br/>
         </CardText>
       </Card>
       <br/>
-        <Card>
-        <CardHeader
-          title="GraphiQL"
-          subtitle="Explore the GraphQL server, anonymously. All links open in a new window."
-        />
-        <CardText>
-          <a href="/graphql" target="_blank">GraphiQL browser</a>
-          <br/>
-          <a href="/graphql?query=query%20ViewerQueries%20%7B%0A%20%20Viewer%20%7B%0A%20%20id%0A%20%20User_DisplayName%0A%20%20User_ProfilePhoto%0A%20%20User_Email%0A%20%20User_Locale%0A%20%20%7D%0A%7D%0A" target="_blank">Simple query requesting some of the viewer object type fields</a>
-          <br/>
-          <a href="/graphql?query=query%20ViewerQueries%20%7B%0A%20%20Viewer%20%7B%0A%20%20%20%20id%0A%20%20%20%20Ensayos(first%3A%202147483647)%20%7B%0A%20%20%20%20%20%20edges%20%7B%0A%20%20%20%20%20%20%20%20node%20%7B%0A%20%20%20%20%20%20%20%20%20%20id%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Title%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Description%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Content%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A" target="_blank">Titles of the essays available in the system</a>
-          <br/>
-          <a href="/graphql?query=query%20IntrospectionQuery%20%7B%0A%20%20__schema%20%7B%0A%20%20%20%20queryType%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%7D%0A%20%20%20%20mutationType%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%7D%0A%20%20%20%20types%20%7B%0A%20%20%20%20%20%20...FullType%0A%20%20%20%20%7D%0A%20%20%20%20directives%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20description%0A%20%20%20%20%20%20args%20%7B%0A%20%20%20%20%20%20%20%20...InputValue%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20onOperation%0A%20%20%20%20%20%20onFragment%0A%20%20%20%20%20%20onField%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A%0Afragment%20FullType%20on%20__Type%20%7B%0A%20%20kind%0A%20%20name%0A%20%20description%0A%20%20fields%20%7B%0A%20%20%20%20name%0A%20%20%20%20description%0A%20%20%20%20args%20%7B%0A%20%20%20%20%20%20...InputValue%0A%20%20%20%20%7D%0A%20%20%20%20type%20%7B%0A%20%20%20%20%20%20...TypeRef%0A%20%20%20%20%7D%0A%20%20%20%20isDeprecated%0A%20%20%20%20deprecationReason%0A%20%20%7D%0A%20%20inputFields%20%7B%0A%20%20%20%20...InputValue%0A%20%20%7D%0A%20%20interfaces%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%20%20enumValues%20%7B%0A%20%20%20%20name%0A%20%20%20%20description%0A%20%20%20%20isDeprecated%0A%20%20%20%20deprecationReason%0A%20%20%7D%0A%20%20possibleTypes%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%7D%0A%0Afragment%20InputValue%20on%20__InputValue%20%7B%0A%20%20name%0A%20%20description%0A%20%20type%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%20%20defaultValue%0A%7D%0A%0Afragment%20TypeRef%20on%20__Type%20%7B%0A%20%20kind%0A%20%20name%0A%20%20ofType%20%7B%0A%20%20%20%20kind%0A%20%20%20%20name%0A%20%20%20%20ofType%20%7B%0A%20%20%20%20%20%20kind%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20ofType%20%7B%0A%20%20%20%20%20%20%20%20kind%0A%20%20%20%20%20%20%20%20name%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A" target="_blank">Self-introspection query</a>
-        </CardText>
-      </Card>
       <Card>
         <CardHeader
           title="Isomorphic Variables"
@@ -93,6 +45,25 @@ class Home_Screen extends React.Component
     </div> );
   }
 }
+
+// https://github.com/codefoundries/UniversalRelayBoilerplate/issues/28
+/*
+<Card>
+  <CardHeader
+    title="GraphiQL"
+    subtitle="Explore the GraphQL server, anonymously. All links open in a new window."
+  />
+  <CardText>
+    <a href="/graphql" target="_blank">GraphiQL browser</a>
+    <br/>
+    <a href="/graphql?query=query%20ViewerQueries%20%7B%0A%20%20Viewer%20%7B%0A%20%20id%0A%20%20User_DisplayName%0A%20%20User_ProfilePhoto%0A%20%20User_Email%0A%20%20User_Locale%0A%20%20%7D%0A%7D%0A" target="_blank">Simple query requesting some of the viewer object type fields</a>
+    <br/>
+    <a href="/graphql?query=query%20ViewerQueries%20%7B%0A%20%20Viewer%20%7B%0A%20%20%20%20id%0A%20%20%20%20Ensayos(first%3A%202147483647)%20%7B%0A%20%20%20%20%20%20edges%20%7B%0A%20%20%20%20%20%20%20%20node%20%7B%0A%20%20%20%20%20%20%20%20%20%20id%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Title%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Description%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Content%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A" target="_blank">Titles of the essays available in the system</a>
+    <br/>
+    <a href="/graphql?query=query%20IntrospectionQuery%20%7B%0A%20%20__schema%20%7B%0A%20%20%20%20queryType%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%7D%0A%20%20%20%20mutationType%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%7D%0A%20%20%20%20types%20%7B%0A%20%20%20%20%20%20...FullType%0A%20%20%20%20%7D%0A%20%20%20%20directives%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20description%0A%20%20%20%20%20%20args%20%7B%0A%20%20%20%20%20%20%20%20...InputValue%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20onOperation%0A%20%20%20%20%20%20onFragment%0A%20%20%20%20%20%20onField%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A%0Afragment%20FullType%20on%20__Type%20%7B%0A%20%20kind%0A%20%20name%0A%20%20description%0A%20%20fields%20%7B%0A%20%20%20%20name%0A%20%20%20%20description%0A%20%20%20%20args%20%7B%0A%20%20%20%20%20%20...InputValue%0A%20%20%20%20%7D%0A%20%20%20%20type%20%7B%0A%20%20%20%20%20%20...TypeRef%0A%20%20%20%20%7D%0A%20%20%20%20isDeprecated%0A%20%20%20%20deprecationReason%0A%20%20%7D%0A%20%20inputFields%20%7B%0A%20%20%20%20...InputValue%0A%20%20%7D%0A%20%20interfaces%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%20%20enumValues%20%7B%0A%20%20%20%20name%0A%20%20%20%20description%0A%20%20%20%20isDeprecated%0A%20%20%20%20deprecationReason%0A%20%20%7D%0A%20%20possibleTypes%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%7D%0A%0Afragment%20InputValue%20on%20__InputValue%20%7B%0A%20%20name%0A%20%20description%0A%20%20type%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%20%20defaultValue%0A%7D%0A%0Afragment%20TypeRef%20on%20__Type%20%7B%0A%20%20kind%0A%20%20name%0A%20%20ofType%20%7B%0A%20%20%20%20kind%0A%20%20%20%20name%0A%20%20%20%20ofType%20%7B%0A%20%20%20%20%20%20kind%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20ofType%20%7B%0A%20%20%20%20%20%20%20%20kind%0A%20%20%20%20%20%20%20%20name%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A" target="_blank">Self-introspection query</a>
+  </CardText>
+</Card>
+*/
 
 export default Relay.createContainer( Home_Screen, {
   fragments: {
