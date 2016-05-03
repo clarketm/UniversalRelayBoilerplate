1541fb5a544d826901bf65bcee6d19a6a26bf27a
Alex Chalakov <a.chalakov@codefoundries.com>
2016-05-02 17:41:50 -0700

    Added one more theme

--- configuration/webapp/muiTheme.js
+++ configuration/webapp/muiTheme.js
@@ -1,6 +1,6 @@
 /* @flow weak */
 
 // Change the theme file name here to switch themes:
-import muiTheme from '../../webapp/mui-themes/lightBase'
+import muiTheme from '../../webapp/mui-themes/grayBlue'
 
 export default muiTheme;
---
+++ webapp/mui-themes/purpleOrange.js
@@ -0,0 +1,47 @@
+/* @flow weak */
+
+// This is simply a copy of the light theme as per:
+// http://www.material-ui.com/v0.15.0-alpha.1/#/customization/themes
+
+import {
+  darkBlack,
+  grey100,
+  grey300,
+  grey500,
+  lightBlack,
+  purple900,
+  yellow900,
+  white
+} from 'material-ui/styles/colors';
+import {fade} from 'material-ui/utils/colorManipulator';
+
+export default {
+  spacing: {
+    iconSize: 24,
+    desktopGutter: 24,
+    desktopGutterMore: 32,
+    desktopGutterLess: 16,
+    desktopGutterMini: 8,
+    desktopKeylineIncrement: 64,
+    desktopDropDownMenuItemHeight: 32,
+    desktopDropDownMenuFontSize: 15,
+    desktopLeftNavMenuItemHeight: 48,
+    desktopSubheaderHeight: 48,
+    desktopToolbarHeight: 56,
+  },
+  fontFamily: 'Roboto, sans-serif',
+  palette: {
+    primary1Color: purple900,
+    primary2Color: purple900,
+    primary3Color: lightBlack,
+    accent1Color: yellow900,
+    accent2Color: grey100,
+    accent3Color: grey500,
+    textColor: darkBlack,
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    disabledColor: fade( darkBlack, 0.3 ),
+  },
+  avatar: { borderColor: null },
+};
