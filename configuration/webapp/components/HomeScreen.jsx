c6a5f49d55c77b729016c476e3bf7a8f87fd05e9
Alex Chalakov <a.chalakov@codefoundries.com>
2016-05-17 19:36:42 -0700

    Provide better mechanism to access /graphql. Close #4

--- configuration/webapp/components/HomeScreen.jsx
+++ configuration/webapp/components/HomeScreen.jsx
@@ -57,28 +57,27 @@ class HomeScreen extends React.Component
           <p>PUBLIC_URL: { isoVars.PUBLIC_URL }</p>
         </CardText>
       </Card>
+      <br/>
+      <Card>
+        <CardHeader
+          title="GraphiQL"
+          subtitle="Explore the GraphQL server, anonymously. All links open in a new window."
+        />
+        <CardText>
+          Please notice that the links below will work only when you set the <b>USER_TOKEN_2_BYPASS_IP</b> to be your browser's IP address. All other IPs will not be able to authenticate for security reasons. Use that option with caution. It is best if it is set to 127.0.0.1 as by default.
+          <br/>
+          <a href="/graphql" target="_blank">GraphiQL browser</a>
+          <br/>
+          <a href="/graphql?query=query%20ViewerQueries%20%7B%0A%20%20Viewer%20%7B%0A%20%20id%0A%20%20User_DisplayName%0A%20%20User_ProfilePhoto%0A%20%20User_Email%0A%20%20User_Locale%0A%20%20%7D%0A%7D%0A" target="_blank">Simple query requesting some of the viewer object type fields</a>
+          <br/>
+          <a href="/graphql?query=query%20ViewerQueries%20%7B%0A%20%20Viewer%20%7B%0A%20%20%20%20id%0A%20%20%20%20Ensayos(first%3A%202147483647)%20%7B%0A%20%20%20%20%20%20edges%20%7B%0A%20%20%20%20%20%20%20%20node%20%7B%0A%20%20%20%20%20%20%20%20%20%20id%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Title%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Description%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Content%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A" target="_blank">Titles of the essays available in the system</a>
+          <br/>
+          <a href="/graphql?query=query%20IntrospectionQuery%20%7B%0A%20%20__schema%20%7B%0A%20%20%20%20queryType%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%7D%0A%20%20%20%20mutationType%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%7D%0A%20%20%20%20types%20%7B%0A%20%20%20%20%20%20...FullType%0A%20%20%20%20%7D%0A%20%20%20%20directives%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20description%0A%20%20%20%20%20%20args%20%7B%0A%20%20%20%20%20%20%20%20...InputValue%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20onOperation%0A%20%20%20%20%20%20onFragment%0A%20%20%20%20%20%20onField%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A%0Afragment%20FullType%20on%20__Type%20%7B%0A%20%20kind%0A%20%20name%0A%20%20description%0A%20%20fields%20%7B%0A%20%20%20%20name%0A%20%20%20%20description%0A%20%20%20%20args%20%7B%0A%20%20%20%20%20%20...InputValue%0A%20%20%20%20%7D%0A%20%20%20%20type%20%7B%0A%20%20%20%20%20%20...TypeRef%0A%20%20%20%20%7D%0A%20%20%20%20isDeprecated%0A%20%20%20%20deprecationReason%0A%20%20%7D%0A%20%20inputFields%20%7B%0A%20%20%20%20...InputValue%0A%20%20%7D%0A%20%20interfaces%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%20%20enumValues%20%7B%0A%20%20%20%20name%0A%20%20%20%20description%0A%20%20%20%20isDeprecated%0A%20%20%20%20deprecationReason%0A%20%20%7D%0A%20%20possibleTypes%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%7D%0A%0Afragment%20InputValue%20on%20__InputValue%20%7B%0A%20%20name%0A%20%20description%0A%20%20type%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%20%20defaultValue%0A%7D%0A%0Afragment%20TypeRef%20on%20__Type%20%7B%0A%20%20kind%0A%20%20name%0A%20%20ofType%20%7B%0A%20%20%20%20kind%0A%20%20%20%20name%0A%20%20%20%20ofType%20%7B%0A%20%20%20%20%20%20kind%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20ofType%20%7B%0A%20%20%20%20%20%20%20%20kind%0A%20%20%20%20%20%20%20%20name%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A" target="_blank">Self-introspection query</a>
+        </CardText>
+      </Card>
     </div> );
   }
 }
-
-// https://github.com/codefoundries/UniversalRelayBoilerplate/issues/28
-/*
-<Card>
-  <CardHeader
-    title="GraphiQL"
-    subtitle="Explore the GraphQL server, anonymously. All links open in a new window."
-  />
-  <CardText>
-    <a href="/graphql" target="_blank">GraphiQL browser</a>
-    <br/>
-    <a href="/graphql?query=query%20ViewerQueries%20%7B%0A%20%20Viewer%20%7B%0A%20%20id%0A%20%20User_DisplayName%0A%20%20User_ProfilePhoto%0A%20%20User_Email%0A%20%20User_Locale%0A%20%20%7D%0A%7D%0A" target="_blank">Simple query requesting some of the viewer object type fields</a>
-    <br/>
-    <a href="/graphql?query=query%20ViewerQueries%20%7B%0A%20%20Viewer%20%7B%0A%20%20%20%20id%0A%20%20%20%20Ensayos(first%3A%202147483647)%20%7B%0A%20%20%20%20%20%20edges%20%7B%0A%20%20%20%20%20%20%20%20node%20%7B%0A%20%20%20%20%20%20%20%20%20%20id%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Title%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Description%0A%20%20%20%20%20%20%20%20%20%20Ensayo_Content%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A" target="_blank">Titles of the essays available in the system</a>
-    <br/>
-    <a href="/graphql?query=query%20IntrospectionQuery%20%7B%0A%20%20__schema%20%7B%0A%20%20%20%20queryType%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%7D%0A%20%20%20%20mutationType%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%7D%0A%20%20%20%20types%20%7B%0A%20%20%20%20%20%20...FullType%0A%20%20%20%20%7D%0A%20%20%20%20directives%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20description%0A%20%20%20%20%20%20args%20%7B%0A%20%20%20%20%20%20%20%20...InputValue%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20onOperation%0A%20%20%20%20%20%20onFragment%0A%20%20%20%20%20%20onField%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A%0Afragment%20FullType%20on%20__Type%20%7B%0A%20%20kind%0A%20%20name%0A%20%20description%0A%20%20fields%20%7B%0A%20%20%20%20name%0A%20%20%20%20description%0A%20%20%20%20args%20%7B%0A%20%20%20%20%20%20...InputValue%0A%20%20%20%20%7D%0A%20%20%20%20type%20%7B%0A%20%20%20%20%20%20...TypeRef%0A%20%20%20%20%7D%0A%20%20%20%20isDeprecated%0A%20%20%20%20deprecationReason%0A%20%20%7D%0A%20%20inputFields%20%7B%0A%20%20%20%20...InputValue%0A%20%20%7D%0A%20%20interfaces%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%20%20enumValues%20%7B%0A%20%20%20%20name%0A%20%20%20%20description%0A%20%20%20%20isDeprecated%0A%20%20%20%20deprecationReason%0A%20%20%7D%0A%20%20possibleTypes%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%7D%0A%0Afragment%20InputValue%20on%20__InputValue%20%7B%0A%20%20name%0A%20%20description%0A%20%20type%20%7B%0A%20%20%20%20...TypeRef%0A%20%20%7D%0A%20%20defaultValue%0A%7D%0A%0Afragment%20TypeRef%20on%20__Type%20%7B%0A%20%20kind%0A%20%20name%0A%20%20ofType%20%7B%0A%20%20%20%20kind%0A%20%20%20%20name%0A%20%20%20%20ofType%20%7B%0A%20%20%20%20%20%20kind%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20ofType%20%7B%0A%20%20%20%20%20%20%20%20kind%0A%20%20%20%20%20%20%20%20name%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A" target="_blank">Self-introspection query</a>
-  </CardText>
-</Card>
-*/
 
 export default Relay.createContainer( HomeScreen, {
   fragments: {
--- server/credentials_check.js
+++ server/credentials_check.js
@@ -1,29 +1,29 @@
 /* @flow weak */
 
-import jwt from 'jwt-simple';
+import jwt from 'jwt-simple'
 
-import log from './log.js';
+import log from './log.js'
 
 
 // Read environment
-require( 'dotenv' ).load( );
+require( 'dotenv' ).load( )
 
 export function getUserByCookie( objectManager, req)
 {
-  let user_id = '00000000-0000-0000-0000-000000000000'; // Anonymous, unless cookie is passed
+  let user_id = '00000000-0000-0000-0000-000000000000' // Anonymous, unless cookie is passed
 
   try
   {
     if( req.cookies.user_token_1 )
       if( req.cookies.user_token_1.length > 10 )
       {
-        var decoded = jwt.decode( req.cookies.user_token_1, process.env.JWT_SECRET );
-        user_id = decoded.user_id;
+        var decoded = jwt.decode( req.cookies.user_token_1, process.env.JWT_SECRET )
+        user_id = decoded.user_id
       }
   }
   catch( err )
   {
-    return Promise.reject( "Could not read auth cookie. " + err );
+    return Promise.reject( "Could not read auth cookie. " + err )
   }
 
   return objectManager.getOneById( 'User', user_id )
@@ -34,31 +34,31 @@ export function getUserByCookie( objectManager, req)
       return a_User
     }
     else
-      return Promise.reject( "User not found" );
+      return Promise.reject( "User not found" )
   } )
-  ;
+
 }
 
 export function verifyUserAuthToken( a_User, req )
 {
   if ( ! a_User )
-    return Promise.reject( "User not found" );
+    return Promise.reject( "User not found" )
   else
   {
-    const request_User_Token2 = req.get( 'user_token_2' );
-    if( request_User_Token2 == a_User.User_Token2 )
-      return Promise.resolve( a_User.id );
+    const request_User_Token2 = req.get( 'user_token_2' )
+    if( request_User_Token2 == a_User.User_Token2 || process.env.USER_TOKEN_2_BYPASS_IP == req.ip )
+      return Promise.resolve( a_User.id )
     else
     {
-      return Promise.reject( "Authentication token expected: " + a_User.User_Token2 + ", provided:" + request_User_Token2 );
+      return Promise.reject( "Authentication token expected: " + a_User.User_Token2 + ", provided:" + request_User_Token2 )
     }
   }
 }
 
 export function serveAuthenticationFailed( res, message )
 {
-  log.log( 'warn', 'Checking credentials failed: ' + message );
+  log.log( 'warn', 'Checking credentials failed: ' + message )
 
-  res.cookie( 'user_token_1', '', { httpOnly: true, expires: new Date( 1 ) } ); // Expire cookie. Only way to 'delete'
-  res.status( 403 ).send( 'Authentication failed' );
+  res.cookie( 'user_token_1', '', { httpOnly: true, expires: new Date( 1 ) } ) // Expire cookie. Only way to 'delete'
+  res.status( 403 ).send( 'Authentication failed' )
 }
