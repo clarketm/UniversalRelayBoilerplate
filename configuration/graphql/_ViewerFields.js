014b0165870aae59dbfe2e6e79d5c616e30a75f5
Alex Chalakov <a.chalakov@codefoundries.com>
2016-05-24 21:55:22 -0700

    Reorganize naming of modules in _mutations and _ViewerFields. Close #90

--- configuration/graphql/_ViewerFields.js
+++ configuration/graphql/_ViewerFields.js
@@ -1,14 +1,14 @@
 /* @flow */
 
-import Compendium_ViewerFields from '../../units/urb-example-compendium/graphql/type/_ViewerFields'
-import Ensayo_ViewerFields from '../../units/urb-example-ensayo/graphql/type/_ViewerFields'
-import ToDo_ViewerFields from '../../units/urb-example-todo/graphql/type/_ViewerFields'
-import Translaticiarum_ViewerFields from '../../units/urb-example-translaticiarum/graphql/type/_ViewerFields'
+import urb_example_compendium_ViewerFields from '../../units/urb-example-compendium/graphql/type/_ViewerFields'
+import urb_example_ensayo_ViewerFields from '../../units/urb-example-ensayo/graphql/type/_ViewerFields'
+import urb_example_todo_ViewerFields from '../../units/urb-example-todo/graphql/type/_ViewerFields'
+import urb_example_translaticiarum_ViewerFields from '../../units/urb-example-translaticiarum/graphql/type/_ViewerFields'
 
 
 export default {
-  ...Compendium_ViewerFields,
-  ...Ensayo_ViewerFields,
-  ...ToDo_ViewerFields,
-  ...Translaticiarum_ViewerFields,
+  ...urb_example_compendium_ViewerFields,
+  ...urb_example_ensayo_ViewerFields,
+  ...urb_example_todo_ViewerFields,
+  ...urb_example_translaticiarum_ViewerFields,
 }
--- configuration/graphql/_mutations.js
+++ configuration/graphql/_mutations.js
@@ -1,17 +1,18 @@
 /* @flow */
 
-import UserManagement_mutations from '../../units/urb-account-management/graphql/mutation/_mutations'
+import urb_account_management_mutations from '../../units/urb-account-management/graphql/mutation/_mutations'
 
-import Compendium_mutations from '../../units/urb-example-compendium/graphql/mutation/_mutations'
-import Ensayo_mutations from '../../units/urb-example-ensayo/graphql/mutation/_mutations'
-import ToDo_mutations from '../../units/urb-example-todo/graphql/mutation/_mutations'
-import Translaticiarum_mutations from '../../units/urb-example-translaticiarum/graphql/mutation/_mutations'
+import urb_example_compendium_mutations from '../../units/urb-example-compendium/graphql/mutation/_mutations'
+import urb_example_ensayo_mutations from '../../units/urb-example-ensayo/graphql/mutation/_mutations'
+import urb_example_todo_mutations from '../../units/urb-example-todo/graphql/mutation/_mutations'
+import urb_example_translaticiarum_mutations from '../../units/urb-example-translaticiarum/graphql/mutation/_mutations'
 
 
 export default {
-  ...UserManagement_mutations,
-  ...Compendium_mutations,
-  ...Ensayo_mutations,
-  ...ToDo_mutations,
-  ...Translaticiarum_mutations,
+  ...urb_account_management_mutations,
+
+  ...urb_example_compendium_mutations,
+  ...urb_example_ensayo_mutations,
+  ...urb_example_todo_mutations,
+  ...urb_example_translaticiarum_mutations,
 }
