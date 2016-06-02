d53de4ca3333062a0e45815552690cf4f7985885
Alex Chalakov <a.chalakov@codefoundries.com>
2016-06-02 10:56:33 -0700

    React Native application is too small. Close #102

--- app/components/ApplicationMain.js
+++ app/components/ApplicationMain.js
@@ -8,9 +8,7 @@ import routes from '../../configuration/app/routes'
 const styles = StyleSheet.create( {
   container: {
     flex:1,
-    backgroundColor:"transparent",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor:"red",
   },
 } )
 
@@ -24,9 +22,8 @@ function getSceneStyle ( props )
 {
   return {
     flex: 1,
-    marginTop: props.hideNavBar ? 0 : 64,
-    marginBottom: props.hideTabBar ? 0 : 49.5,
-    backgroundColor: '#fff',
+    marginTop: 0,
+    backgroundColor: '#0000ff',
     shadowColor: null,
     shadowOffset: null,
     shadowOpacity: null,
@@ -60,14 +57,19 @@ export default class ApplicationMain extends React.Component
 {
   render( )
   {
-    return <Router createReducer={reducerCreate} getSceneStyle={getSceneStyle}>
-      <Scene key="modal" component={Modal} >
-        <Scene key="root" hideNavBar={true} hideTabBar={true}>
-          <Scene key="tabbar" component={NavigationDrawer} initial={true}>
-            { routes( MenuButton ) }
-          </Scene>
+    return <View style={styles.container}>
+      <Router createReducer={reducerCreate} getSceneStyle={getSceneStyle}>
+        <Scene key="modal" component={Modal} >
+            <Scene key="tabbar" component={NavigationDrawer} initial={true}>
+              { routes( MenuButton ) }
+            </Scene>
         </Scene>
-      </Scene>
-    </Router>
+      </Router>
+    </View>
   }
 }
+
+/*
+<Scene key="root" hideNavBar={true} hideTabBar={true}>
+</Scene>
+*/
