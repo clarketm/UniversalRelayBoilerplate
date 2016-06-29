8192817c92175b9b8401e4a51fa327e650203ffe
Alex Chalakov <a.chalakov@codefoundries.com>
2016-06-28 19:41:42 -0700

    Provide correct index.android.js

--- index.android.js
+++ index.android.js
@@ -1,52 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
-import React, { Component } from 'react';
-import {
+import React, {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
 } from 'react-native';
 
-class UniversalRelayBoilerplate extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import ApplicationMain from './app/components/ApplicationMain';
+import AppRegistryName from './configuration/app/AppRegistryName';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('UniversalRelayBoilerplate', () => UniversalRelayBoilerplate);
+AppRegistry.registerComponent( AppRegistryName, () => ApplicationMain );
