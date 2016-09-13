f1ca704e039c4ae290f13c900a749b5609a58cec
Alex Chalakov <a.chalakov@codefoundries.com>
2016-09-13 16:43:13 -0700

    Fixes to Android stuff and re-linking of all native libraries

--- android/app/src/main/java/com/universalrelayboilerplate/MainApplication.java
+++ android/app/src/main/java/com/universalrelayboilerplate/MainApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.keychain.KeychainPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.keychain.KeychainPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new KeychainPackage(),
+            new RNDeviceInfo(),
             new KeychainPackage()
       );
     }
