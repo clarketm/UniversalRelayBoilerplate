d499123eacd42bbd829672ca4d2d523d2a48f07e
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2017-07-31 02:21:49 -0700

    [server] server public_files from configuration. Close #329

---
+++ units/urb-base-server/healthz.js
@@ -0,0 +1,18 @@
+// @flow
+
+import express from 'express'
+
+import defaultPersister from '../_configuration/urb-base-server/graphql/defaultPersister'
+
+const healthz = express()
+
+healthz.get('/', async (req, res, next) => {
+  try {
+    await defaultPersister.confirmHealth()
+    res.sendStatus(200)
+  } catch (err) {
+    next(new Error('💔 DB is unreachable'))
+  }
+})
+
+export default healthz
