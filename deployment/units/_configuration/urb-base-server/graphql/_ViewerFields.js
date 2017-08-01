b40146509b867859e37c45bbd42d5d0c97d4c9e8
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2017-07-31 20:45:30 -0700

    Fixes to build-server to copy all ejs files and preserve .graphql.js files

--- deployment/units/_configuration/urb-base-server/graphql/_ViewerFields.js
+++
@@ -1,5 +0,0 @@
-Object.defineProperty(exports,"__esModule",{value:true});var _extends=Object.assign||function(target){for(var i=1;i<arguments.length;i++){var source=arguments[i];for(var key in source){if(Object.prototype.hasOwnProperty.call(source,key)){target[key]=source[key];}}}return target;};
-
-var _ViewerFields=require('../../../urb-example-ensayo-server/graphql/type/_ViewerFields');var _ViewerFields2=_interopRequireDefault(_ViewerFields);
-var _ViewerFields3=require('../../../urb-example-todo-server/graphql/type/_ViewerFields');var _ViewerFields4=_interopRequireDefault(_ViewerFields3);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.default=_extends({},_ViewerFields2.default,_ViewerFields4.default);
-//# sourceMappingURL=_ViewerFields.js.map
--- deployment/units/urb-base-server/authExtensions.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-export default function(auth) {
-  // Here [auth] is the auth router. If you need to attach additional routes to it, do it below
-}
