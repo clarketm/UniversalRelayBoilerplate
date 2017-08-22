424dc0f269b650a6f595fad6b2f0fda706d8da03
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2017-08-22 03:29:14 -0700

    [units/usb-base-webapp] Move authentication screens to separate unit, with async routes. Close #384

---
+++ units/urb-account-management-webapp/routerAppFrameAccountManagement.jsx
@@ -0,0 +1,18 @@
+// @flow
+
+import Async from 'react-code-splitting'
+import { graphql } from 'react-relay'
+import React from 'react'
+import Route from 'found/lib/Route'
+
+const LogoutScreen = props =>
+  <Async load={import('./components/LogoutScreen')} componentProps={props} />
+const NewUserScreen = props =>
+  <Async load={import('./components/NewUserScreen')} componentProps={props} />
+
+export default (
+  <Route path="user">
+    <Route path="new" Component={NewUserScreen} />
+    <Route path="logout" Component={LogoutScreen} />
+  </Route>
+)
