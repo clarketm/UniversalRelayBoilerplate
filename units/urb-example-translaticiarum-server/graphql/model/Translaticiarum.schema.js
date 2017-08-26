47ea712c8cd967db037f8c25058d133ade9df804
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2017-08-25 20:19:25 -0700

    Test coma dangle

--- units/urb-example-translaticiarum-server/graphql/model/Translaticiarum.schema.js
+++ units/urb-example-translaticiarum-server/graphql/model/Translaticiarum.schema.js
@@ -1,22 +1,22 @@
 // @flow
 
-import defaultPersister from "../../../_configuration/urb-base-server/graphql/defaultPersister"
+import defaultPersister from '../../../_configuration/urb-base-server/graphql/defaultPersister'
 
-defaultPersister.addTableSchema( "Translaticiarum", {
+defaultPersister.addTableSchema( 'Translaticiarum', {
   fields: {
-    id: "uuid",
-    Translaticiarum_User_id: "uuid",
-    Translaticiarum_Stop: "timestamp",
-    Translaticiarum_Start: "timestamp",
-    Translaticiarum_Description: "text",
+    id: 'uuid',
+    Translaticiarum_User_id: 'uuid',
+    Translaticiarum_Stop: 'timestamp',
+    Translaticiarum_Start: 'timestamp',
+    Translaticiarum_Description: 'text',
   },
 
-  key: [ "id" ],
+  key: [ 'id' ],
 
   custom_indexes: [
     {
-      on: "Translaticiarum_User_id",
-      using: "org.apache.cassandra.index.sasi.SASIIndex",
+      on: 'Translaticiarum_User_id',
+      using: 'org.apache.cassandra.index.sasi.SASIIndex',
       options: {},
     },
   ],
