fdf006aeddbf26f6bd5de45f9d79550880719bc1
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2017-08-26 15:16:26 -0700

    Version 0.1.1.36

--- deployment/units/_configuration/package.js
+++ deployment/units/_configuration/package.js
@@ -1,5 +1,5 @@
 Object.defineProperty(exports,"__esModule",{value:true});
 
 var name=exports.name='urb';
-var version=exports.version='0.1.0.34';
+var version=exports.version='0.1.1.36';
 //# sourceMappingURL=package.js.map
