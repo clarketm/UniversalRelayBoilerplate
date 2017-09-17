8d03fda4bf8996d0c725dfc1551ffb30c32e4ece
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2017-09-17 03:15:26 -0700

    Make the public static content expire in one year

--- units/urb-base-server/server.js
+++ units/urb-base-server/server.js
@@ -27,43 +27,27 @@ require( 'dotenv' ).load()
 
 const port = process.env.PORT
 if ( port == null || typeof port !== 'string' )
-  throw new Error(
-    '💔  urb-base-server/server.js requires the environment variable PORT to be set'
-  )
-
+  throw new Error( '💔  urb-base-server/server.js requires the environment variable PORT to be set' )
 const host = process.env.HOST
 if ( host == null || typeof host !== 'string' )
-  throw new Error(
-    '💔  urb-base-server/server.js requires the environment variable HOST to be set'
-  )
-
-// Log startup information
+  throw new Error( '💔  urb-base-server/server.js requires the environment variable HOST to be set' ) // Log startup information
 log.log( 'info', 'Starting application', {
   name: name,
   version: version,
-
   NODE_ENV: process.env.NODE_ENV,
   HOST: process.env.HOST,
   PORT: process.env.PORT,
   PUBLIC_URL: process.env.PUBLIC_URL,
-
   process_title: process.title,
   process_pid: process.pid,
   local_ip: getLocalIP(),
-})
-
-// Main router
-const server = express()
-
-// Add headers
+}) // Main router
+const server = express() // Add headers
 server.use( function( req, res, next ) {
   // Website you wish to allow to connect
   res.setHeader( 'Access-Control-Allow-Origin', process.env.PUBLIC_URL )
   // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  )
+  res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE' )
   // Request headers you wish to allow
   res.setHeader( 'Access-Control-Allow-Headers', 'X-Requested-With,content-type' )
   // Set to true if you need the website to include cookies in the requests sent
@@ -71,42 +55,22 @@ server.use( function( req, res, next ) {
   res.setHeader( 'Access-Control-Allow-Credentials', true )
   // Pass to next layer of middleware
   next()
-})
-
-// Configure main router
+}) // Configure main router
 server.set( 'trust proxy', 'loopback' )
 server.set( 'x-powered-by', false )
 server.use( compression() )
-server.use( cookieParser() )
-
-// GraphQL server
-server.use( '/graphql', graphql )
-
-// Authentication server
-server.use( '/auth', serverAuth )
-
-// Health check endpoint
-server.use( '/healthz', serverHealthz )
-
-// Staticpublic files server
+server.use( cookieParser() ) // GraphQL server
+server.use( '/graphql', graphql ) // Authentication server
+server.use( '/auth', serverAuth ) // Health check endpoint
+server.use( '/healthz', serverHealthz ) // Staticpublic files server
 server.use(
-  express.static(
-    path.resolve(
-      __dirname + '/../_configuration/urb-base-server/public_files/'
-    ),
-    {
-      maxAge: 365 * 86400000, // one year
-    }
-  )
+  express.static( path.resolve( __dirname + '/../_configuration/urb-base-server/public_files/' ), {
+    maxAge: 365 * 86400, // one year
+  }),
 )
-
 // Add extensions - custom routes
-serverExtensions( server )
-
-// Application with routes
-server.use( serverWebApp )
-
-// Set up all persisters
+serverExtensions( server ) // Application with routes
+server.use( serverWebApp ) // Set up all persisters
 ObjectManager.initializePersisters( false, () => {
   // Serve - work differently in development and production. In production only the
   // specified host serves
@@ -116,12 +80,10 @@ ObjectManager.initializePersisters( false, () => {
   } else {
     // Development server - localhost. Always run on localhost
     startDevelopmentServer( port, '127.0.0.1' )
-
     // Development server - on a specific IP, if different from localhost
     if ( host !== '127.0.0.1' ) startDevelopmentServer( port, host )
   }
 })
-
 function startDevelopmentServer( port, host ) {
   const localIPDevelopmentServer = express()
   localIPDevelopmentServer.use( server )
