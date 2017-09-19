742d94e633e5ee40a49dbd643b8930ab7c9b4a2a
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2017-09-19 02:44:01 -0700

    Version 0.4.1.0

--- deployment/units/_configuration/package.js
+++ deployment/units/_configuration/package.js
@@ -1,5 +1,5 @@
 'use strict';Object.defineProperty(exports, "__esModule", { value: true });
 
 const name = exports.name = 'urb';
-const version = exports.version = '0.4.0.1';
+const version = exports.version = '0.4.1.0';
 //# sourceMappingURL=package.js.map
--- deployment/units/_configuration/urb-base-app/publicURL.js
+++ deployment/units/_configuration/urb-base-app/publicURL.js
@@ -1,6 +1,6 @@
 'use strict';Object.defineProperty(exports, "__esModule", { value: true });
 
-const publicURL = 'http://172.31.99.247:26005';exports.default =
+const publicURL = 'http://universal-relay-boilerplate.herokuapp.com';exports.default =
 
 publicURL;
 //# sourceMappingURL=publicURL.js.map
